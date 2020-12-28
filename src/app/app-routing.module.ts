9ff613c5a0878a7b2e823a68f062cc04b132da38
Bruno Silva <brunosilva2365@gmail.com>
2020-12-27 22:30:55 -0300

    Layout da página de encontros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('../pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'meetings',
+    loadChildren: () => import('./pages/meetings/meetings.module').then( m => m.MeetingsPageModule)
   },
 ];
 
