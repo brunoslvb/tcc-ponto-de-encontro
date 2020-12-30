ea21f78b7cb20be58395f6b72f28e72487723077
Bruno Silva <bruno.silva@edi-labs.com>
2020-12-30 10:29:51 -0300

    Meetings cards getting firebase information

--- src/app/pages/meetings/meetings.page.ts
+++ src/app/pages/meetings/meetings.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ModalMapComponent } from 'src/app/components/modal-map/modal-map.component';
+import { IMeeting } from 'src/app/interfaces/meeting';
+import { MeetingsService } from 'src/app/services/meetings/meetings.service';
 
 @Component({
   selector: 'app-meetings',
@@ -9,17 +11,45 @@ import { ModalMapComponent } from 'src/app/components/modal-map/modal-map.compon
 })
 export class MeetingsPage implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  meetings: Array<IMeeting>;
+
+  constructor(
+    private modalController: ModalController,
+    private service: MeetingsService
+  ) { }
 
   ngOnInit() {
+    this.getAllMeetings();
   }
 
-  async showModalMap(){
+  async showModalMap(meeting: IMeeting){
     const modal = await this.modalController.create({
-      component: ModalMapComponent
+      component: ModalMapComponent,
+      componentProps: { name: meeting.name }
     });
 
     await modal.present();
+  }
+
+  async getAllMeetings(){
+
+    (await this.service.getAllMeetings()).subscribe(data => {
+      
+      this.meetings = data.map(e => {
+      
+        return {
+          id: e.payload.doc.id,
+          name: e.payload.doc.data()['name'],
+          address: e.payload.doc.data()['address'],
+          date: e.payload.doc.data()['date'],
+          time: e.payload.doc.data()['time'],
+        };
+      
+      });
+      
+      console.log(this.meetings);
+    
+    });
   }
 
 }
