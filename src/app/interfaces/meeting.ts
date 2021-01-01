9dbabbac080ec7d812a8cb84cd74d550e0054475
Bruno Silva <bruno.silva@edi-labs.com>
2020-12-31 21:04:26 -0300

    Update meeting interface

--- src/app/interfaces/meeting.ts
+++ src/app/interfaces/meeting.ts
@@ -1,7 +1,13 @@
 export interface IMeeting {
   id: string;
   name: string;
-  address: string;
+  location: {
+    address: string;
+    latitude: number;
+    longitude: number;
+  }
   date: string;
   time: string;
+  members: Array<string>;
+  numberOfmembers: number;
 }
