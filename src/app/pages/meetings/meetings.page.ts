3c9e7d714be32b93813fe4feb077e8aa3f02aca8
Bruno Silva <brunosilva2365@gmail.com>
2021-01-02 17:11:12 -0300

    Bug correction when edit meeting

--- src/app/pages/meetings/meetings.page.ts
+++ src/app/pages/meetings/meetings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalNotifications } from '@capacitor/core';
 import { ModalController } from '@ionic/angular';
 import { ModalMapComponent } from 'src/app/components/modal-map/modal-map.component';
 import { IMeeting } from 'src/app/interfaces/meeting';
@@ -36,20 +37,31 @@ export class MeetingsPage implements OnInit {
   async getMeetingsOfUser(){
     
     this.userService.getById(sessionStorage.getItem('user')).subscribe((user: any) => {
-
+      
       let meetingsAux = [];
-
+      
       user.payload.data().groups.forEach(group => {
+
         this.meetingService.getById(group).subscribe(doc => {
+
           let data: any = doc.payload.data();
 
           data.id = doc.payload.id;
 
-          meetingsAux.push(data);
+          let aux = meetingsAux.findIndex(meeting => meeting.id === data.id);
+
+          if(aux != -1){
+            meetingsAux[aux] = data;
+          } else {
+            meetingsAux.push(data);
+          }
+
         });
+
       });
 
       this.meetings = meetingsAux;
+
     });
   }
 
