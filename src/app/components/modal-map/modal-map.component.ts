edc17fb0839909006fad5fcb0441f9d835d28713
Bruno Silva <brunosilva2365@gmail.com>
2021-01-03 20:58:06 -0300

    Map modal incomplete

--- src/app/components/modal-map/modal-map.component.ts
+++ src/app/components/modal-map/modal-map.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { LoadingController, ModalController, Platform } from '@ionic/angular';
 import { IMeeting } from 'src/app/interfaces/meeting';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions, CameraPosition, MarkerOptions, Marker, Environment } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'app-modal-map',
@@ -9,17 +10,67 @@ import { IMeeting } from 'src/app/interfaces/meeting';
 })
 export class ModalMapComponent implements OnInit {
 
-  // @Input() meeting: IMeeting;
+  @ViewChild('map') mapElement: any; 
+
   @Input() name: string;
 
+  private loading: any;
+  private map: GoogleMap;
+
   constructor(
-    private modalController: ModalController
+    private modalController: ModalController,
+    private loadingController: LoadingController,
+    private platform: Platform
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    this.mapElement.style.width = `${this.platform.width}px`;
+    this.mapElement.style.height = `${this.platform.height}px`;
+
+    this.loadMap();
+  }
 
   async closeModalMap(){
     await this.modalController.dismiss();
+  }
+
+  async loadMap() {
+
+    this.loading = await this.loadingController.create({ message: "Aguarde..." });
+    await this.loading.present();
+
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyCQW7UBbaXZdVAmA0RizRc3XqST8hpUrvQ',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyCQW7UBbaXZdVAmA0RizRc3XqST8hpUrvQ'
+    });
+
+    // let mapOptions: GoogleMapOptions = {
+    //   camera: {
+    //      target: {
+    //        lat: 43.0741904,
+    //        lng: -89.3809802
+    //      },
+    //      zoom: 18,
+    //      tilt: 30
+    //    }
+    // };
+
+    this.map = GoogleMaps.create(this.mapElement);
+
+    // let marker: Marker = this.map.addMarkerSync({
+    //   title: 'Ionic',
+    //   icon: 'blue',
+    //   animation: 'DROP',
+    //   position: {
+    //     lat: 43.0741904,
+    //     lng: -89.3809802
+    //   }
+    // });
+    // marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+    //   alert('clicked');
+    // });
   }
 
 }
